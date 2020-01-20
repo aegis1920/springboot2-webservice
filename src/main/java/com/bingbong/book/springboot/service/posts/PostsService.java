6f38f206cde1b655878ea6a9ace1d8d683b61c0b
aegis1920 <aegis1920@gmail.com>
2020-01-20 18:53:01 +0900

    add : 게시글 등록화면 및 전체 조회 화면

--- src/main/java/com/bingbong/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/bingbong/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.bingbong.book.springboot.service.posts;
 
 import com.bingbong.book.springboot.domain.posts.Posts;
 import com.bingbong.book.springboot.domain.posts.PostsRepository;
+import com.bingbong.book.springboot.web.dto.PostsListResponseDto;
 import com.bingbong.book.springboot.web.dto.PostsResponseDto;
 import com.bingbong.book.springboot.web.dto.PostsSaveRequestDto;
 import com.bingbong.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,6 +39,13 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
 
